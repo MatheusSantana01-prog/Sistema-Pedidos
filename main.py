7e48320e3163a5cecb635a09ee16327453a030e4
MatheusSantana01-prog <matheus.ws@yahoo.com>
2026-04-09 23:23:03 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,88 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
-from supabase import create_client
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_ANON_KEY"))
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/frontend", StaticFiles(directory="../frontend"), name="frontend")
+
+pedidos = []
+contador_id = 1
+
 
 class Pedido(BaseModel):
     mesa: int
     item: str
 
+
 @app.get("/")
 def inicio():
     return {"mensagem": "Servidor do restaurante funcionando"}
 
+
 @app.post("/pedido")
 def criar_pedido(pedido: Pedido):
-    resultado = supabase.table("pedidos").insert({
-        "mesa_id": str(pedido.mesa),
-        "sessao_cliente": f"mesa_{pedido.mesa}",
-        "status": "confirmado",
-        "status_pagamento": "aguardando",
-        "subtotal": 0,
-        "total": 0
-    }).execute()
-    return {"status": "pedido salvo no banco", "dados": resultado.data}
+    global contador_id
+
+    novo_pedido = {
+        "id": contador_id,
+        "mesa": pedido.mesa,
+        "item": pedido.item,
+        "status": "em preparo",
+        "recebido_em": None
+    }
+
+    pedidos.append(novo_pedido)
+    contador_id += 1
+
+    return novo_pedido
+
 
 @app.get("/pedidos")
 def listar_pedidos():
-    resultado = supabase.table("pedidos").select("*").execute()
-    return resultado.data
+    global pedidos
+
+    agora = datetime.now()
+
+    pedidos = [
+        pedido for pedido in pedidos
+        if not (
+            pedido["status"] == "recebido" and
+            pedido["recebido_em"] is not None and
+            agora - pedido["recebido_em"] >= timedelta(minutes=2)
+        )
+    ]
+
+    return pedidos
+
+
+@app.put("/pedido/{pedido_id}")
+def atualizar_status(pedido_id: int):
+    for pedido in pedidos:
+        if pedido["id"] == pedido_id:
+
+            if pedido["status"] == "em preparo":
+                pedido["status"] = "pronto"
+                pedido["recebido_em"] = None
+
+            elif pedido["status"] == "pronto":
+                pedido["status"] = "recebido"
+                pedido["recebido_em"] = datetime.now()
+
+            elif pedido["status"] == "recebido":
+                pedido["status"] = "em preparo"
+                pedido["recebido_em"] = None
+
+            return pedido
+
+    return {"erro": "pedido não encontrado"}
